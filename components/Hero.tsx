2dc4ad994bd79fd972eef3bb24962f7456da21bb
Tahasiraj1 <tahasiraj242@gmail.com>
2024-12-30 01:16:47 +0500

    Refactor Hero component for improved readability and layout consistency

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,63 +1,91 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import { GradientBlob } from './ui/GradientBlob'
-import { motion } from 'framer-motion'
+import React from "react";
+import Image from "next/image";
+import { GradientBlob } from "./ui/GradientBlob";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className='relative flex-col items-center justify-center p-10 overflow-x-clip'>
-        <div className='absolute -top-20 -left-40 opacity-50 -z-10 animate-pulse-slow' >
-            <GradientBlob colors={["#FF00A9", "#00F0FF"]} size={500} blur={60} />
-        </div>
-        <div className='absolute -top-20 right-52 opacity-50 -z-10 animate-pulse-slow' >
-            <GradientBlob colors={["#FF00A9", "#00F0FF"]} size={500} blur={60} />
-        </div>
-        <h1 className='text-3xl md:text-5xl font-bold text-center'>
-            <span className='text-fuchsia-700'>Empower Your Code</span><br /> Tips, Tricks, and Insights for Programmers
-        </h1>
-        <p className='text-lg text-center mt-6'>
-            Learn how to write better code, faster, and with fewer bugs
-        </p>
-        <div className='flex items-center justify-center mt-10 space-x-2 relative'>
-            <motion.div
-            className='rounded-lg aspect-square overflow-hidden'
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{  opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            transition={{  duration: 0.2, type: 'spring', damping: 10, stiffness: 100 }}
-            >
-                <Image src='/images/binary-6285217.jpg' alt='Hero Image' width={400} height={500}
-                className='rounded-lg aspect-square w-[150px] h-[200px] md:w-[400px] md:h-[500px] '
-                />
-            </motion.div>
-            <motion.div
-            className='absolute rounded-lg aspect-square overflow-hidden z-10'
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{  opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{  duration: 0.8, type: 'spring', damping: 10, stiffness: 100 }}
-            // style={{ transform: 'scale(1.1)' }}
-            >
-                <Image src='/images/rahul-mishra-glmeeU0zabw-unsplash.jpg' alt='Hero Image' width={440} height={550}            
-                className='rounded-lg aspect-square  w-[160px] h-[210px] md:w-[440px] md:h-[550px]'
-                />
-            </motion.div>
-            <motion.div
-            className='rounded-lg aspect-square overflow-hidden'
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{  opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            transition={{  duration: 0.2, type: 'spring', damping: 10, stiffness: 100 }}
-            >
-                <Image src='/images/pass2.jpg' alt='Hero Image' width={400} height={500}            
-                className='rounded-lg aspect-square w-[150px] h-[200px] md:w-[400px] md:h-[500px]'
-                />
-            </motion.div>
-        </div>
+    <div className="relative flex-col items-center justify-center p-10 overflow-x-clip">
+      <div className="absolute -top-20 -left-40 opacity-50 -z-10 animate-pulse-slow">
+        <GradientBlob colors={["#FF00A9", "#00F0FF"]} size={500} blur={60} />
+      </div>
+      <div className="absolute -top-20 right-52 opacity-50 -z-10 animate-pulse-slow">
+        <GradientBlob colors={["#FF00A9", "#00F0FF"]} size={500} blur={60} />
+      </div>
+      <h1 className="text-3xl md:text-5xl font-bold text-center">
+        <span className="text-fuchsia-700">Empower Your Code</span>
+        <br /> Tips, Tricks, and Insights for Programmers
+      </h1>
+      <p className="text-lg text-center mt-6">
+        Learn how to write better code, faster, and with fewer bugs
+      </p>
+      <div className="flex items-center justify-center mt-20 space-x-2 relative">
+        <motion.div
+          className="rounded-lg aspect-square overflow-hidden"
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.2,
+            type: "spring",
+            damping: 10,
+            stiffness: 100,
+          }}
+        >
+          <Image
+            src="/images/binary-6285217.jpg"
+            alt="Hero Image"
+            width={400}
+            height={500}
+            className="rounded-lg aspect-square w-[150px] h-[200px] md:w-[400px] md:h-[500px] "
+          />
+        </motion.div>
+        <motion.div
+          className="absolute rounded-lg aspect-square overflow-hidden z-10"
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.8,
+            type: "spring",
+            damping: 10,
+            stiffness: 100,
+          }}
+          // style={{ transform: 'scale(1.1)' }}
+        >
+          <Image
+            src="/images/rahul-mishra-glmeeU0zabw-unsplash.jpg"
+            alt="Hero Image"
+            width={440}
+            height={550}
+            className="rounded-lg aspect-square  w-[160px] h-[210px] md:w-[440px] md:h-[550px]"
+          />
+        </motion.div>
+        <motion.div
+          className="rounded-lg aspect-square overflow-hidden"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.2,
+            type: "spring",
+            damping: 10,
+            stiffness: 100,
+          }}
+        >
+          <Image
+            src="/images/pass2.jpg"
+            alt="Hero Image"
+            width={400}
+            height={500}
+            className="rounded-lg aspect-square w-[150px] h-[200px] md:w-[400px] md:h-[500px]"
+          />
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { RiMenu3Line } from "react-icons/ri";
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -13,111 +13,147 @@ import {
 import { motion } from "framer-motion";
 import DotPattern from "../ui/dot-pattern";
 
-
 const Header = () => {
   return (
-    <div className='relative font-extrabold text-xl w-full h-20 flex items-center justify-between drop-shadow-xl text-black px-4 md:px-8 top-0 z-50 opacity-90'>
-        <DotPattern className="absolute top-0 left-0 -z-10" />
-      <h1 className='font-bold text-2xl animate-in slide-in-from-left-full transition-transform transform duration-1000'>
+    <div className="relative font-extrabold text-xl w-full h-20 flex items-center justify-between drop-shadow-xl text-black px-4 md:px-8 top-0 z-50 opacity-90 backdrop-blur-3xl bg-white/60">
+      <DotPattern className="absolute top-0 left-0 -z-10" />
+      <h1 className="font-bold text-2xl animate-in slide-in-from-left-full transition-transform transform duration-1000">
         NAME
       </h1>
-      <div className='hidden md:block font-semibold'>
-        <ul className='flex'>
-
+      <div className="hidden md:block font-semibold">
+        <ul className="flex">
           <Button variant="linkHover2">
             <Link href="/">
-              <motion.li className='font-semibold text-lg drop-shadow-lg'
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.1 }}
-              >HOME</motion.li>
+              <motion.li
+                className="font-semibold text-lg drop-shadow-lg"
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                HOME
+              </motion.li>
             </Link>
           </Button>
           <Button variant="linkHover2">
             <Link href="/products">
-              <motion.li className='font-semibold text-lg drop-shadow-lg'
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3 }}
-              >BLOGS</motion.li>
+              <motion.li
+                className="font-semibold text-lg drop-shadow-lg"
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                BLOGS
+              </motion.li>
             </Link>
           </Button>
           <Button variant="linkHover2">
             <Link href="/about">
-              <motion.li className='font-semibold text-lg drop-shadow-lg'
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-              >ABOUT</motion.li>
+              <motion.li
+                className="font-semibold text-lg drop-shadow-lg"
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                ABOUT
+              </motion.li>
             </Link>
           </Button>
           <Button variant="linkHover2">
             <Link href="/contact">
-              <motion.li className='font-semibold text-lg drop-shadow-lg'
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.7 }}
-              >CONTACT</motion.li>
+              <motion.li
+                className="font-semibold text-lg drop-shadow-lg"
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7 }}
+              >
+                CONTACT
+              </motion.li>
             </Link>
           </Button>
-
         </ul>
       </div>
-      <div className='flex gap-2 items-center justify-center animate-in slide-in-from-right-full transition-transform transform duration-1000'>
-
+      <div className="flex gap-2 items-center justify-center animate-in slide-in-from-right-full transition-transform transform duration-1000">
         <Sheet>
-
           <SheetTrigger asChild>
-            <Button variant="ghost" size="icon" className="md:hidden bg-transparent hover:bg-transparent">
-              <RiMenu3Line className='w-6 h-6 text-black hover:text-emerald-200' />
+            <Button
+              variant="ghost"
+              size="icon"
+              className="md:hidden bg-transparent hover:bg-transparent"
+            >
+              <RiMenu3Line className="w-6 h-6 text-black hover:text-emerald-200" />
               <span className="sr-only">Open menu</span>
             </Button>
           </SheetTrigger>
-          <SheetContent side="right" className='pt-20 bg-emerald-800 text-white border-gray-600'>
+          <SheetContent
+            side="right"
+            className="pt-20 bg-emerald-800 text-white border-gray-600"
+          >
             <SheetHeader>
               <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
             </SheetHeader>
             <nav>
-
-              <ul className='flex flex-col gap-4'>
+              <ul className="flex flex-col gap-4">
                 <li>
-                  <Button variant="linkHover2" asChild className="w-full justify-start">
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
                     <Link href="/">HOME</Link>
                   </Button>
                 </li>
                 <li>
-                  <Button variant="linkHover2" asChild className="w-full justify-start">
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
                     <Link href="/products">PRODUCTS</Link>
                   </Button>
                 </li>
                 <li>
-                  <Button variant="linkHover2" asChild className="w-full justify-start">
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
                     <Link href="/about">ABOUT</Link>
                   </Button>
                 </li>
                 <li>
-                  <Button variant="linkHover2" asChild className="w-full justify-start">
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
                     <Link href="/contact">CONTACT</Link>
                   </Button>
                 </li>
                 <li>
-                  <Button variant="linkHover2" asChild className="w-full justify-start">
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
                     <Link href="/cart">CART</Link>
                   </Button>
                 </li>
-                  <li>
-                    <Button variant="linkHover2" asChild className="w-full justify-start">
-                      <Link href="/dashboard">DASHBOARD</Link>
-                    </Button>
-                  </li>
-
+                <li>
+                  <Button
+                    variant="linkHover2"
+                    asChild
+                    className="w-full justify-start"
+                  >
+                    <Link href="/dashboard">DASHBOARD</Link>
+                  </Button>
+                </li>
               </ul>
             </nav>
           </SheetContent>
         </Sheet>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
